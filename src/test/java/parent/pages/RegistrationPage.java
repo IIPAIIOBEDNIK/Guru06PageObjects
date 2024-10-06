883cd0803a4a0164f77217605f1926258d3a9efd
3yd <pereponov_alexander@mail.ru>
2024-10-06 18:22:15 +0300

    Added final modification

--- src/test/java/parent/pages/RegistrationPage.java
+++ src/test/java/parent/pages/RegistrationPage.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class RegistrationPage {
-    private SelenideElement firstNameInput = $("#firstName"),
+    private final SelenideElement firstNameInput = $("#firstName"),
             lastNameInput = $("#lastName"),
             userEmailInput = $("#userEmail"),
             genderWrapper = $("#genterWrapper"),
